9ca512a4a5b05ea4932edd4f42e140dd8c9224e8
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-04-27 13:02:43 +0900

    wines_table priceカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :wines
   has_many :favorites, dependent: :destroy
   has_many :comments
+  has_many :cards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_085212) do
+ActiveRecord::Schema.define(version: 2020_04_27_034646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_085212) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "price"
     t.index ["user_id"], name: "index_wines_on_user_id"
   end
 
